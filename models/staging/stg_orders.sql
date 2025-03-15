196aa8a334752ec0bf438913ffc334cd74b01017
Luca Rodi <luca_rodi@it.ibm.com>
2025-03-15 16:28:58 +0000

    developing stage orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,24 @@
+
+select 
+-- from orders
+o.orderid,
+o.orderdate,
+o.shipdate,
+o.shipmode,
+o.ORDERSELLINGPRICE - o.ORDERCOSTPRICE as orderprofit,
+o.ORDERSELLINGPRICE,
+o.ORDERCOSTPRICE,
+-- from products
+p.productid,
+p.productname,
+p.subcategory,
+-- from customers
+c.customername,
+c.customerid,
+c.segment,
+c.country
+from {{ ref('raw_orders') }} as o
+left join {{ ref('raw_product') }} as p 
+on o.productid = p.productid
+left join {{ ref('raw_customer') }} as c
+on o.customerid = c.customerid
